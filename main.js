f6e6e3e7a8993607495679603dc0256574a31f7a
Denis <denis.gordievsky@1win.pro>
2023-05-31 17:49:25 +0700

    fix

--- main.js
+++ main.js
@@ -69,6 +69,47 @@ const reviewSwiper = new Swiper('.reviews-swiper', {
 
 });
 
+const initSwiperPopup = () => {
+    const popupReviewsSwiper = new Swiper('.reviews-popup__wrapper', {
+        slidesPerView: 1,
+        spaceBetween: 24,
+        pagination: {
+            el: '.reviews-popup-pagination',
+            type: 'fraction'
+        },
 
+        navigation: {
+            nextEl: '#popup-reviews-next-button',
+            prevEl: '#popup-reviews-prev-button',
+        },
+    })
+
+    addListenerReviews(popupReviewsSwiper);
+
+}
+
+const addListenerReviews = (swiper) => {
+    const reviews = document.querySelectorAll('[data-number]');
+    const popup = document.querySelector('.reviews-popup');
+
+    reviews.forEach(review => {
+        review.addEventListener('click', function(event) {
+            if (event.target.classList.contains('reviews-card-company')) {
+                return null;
+            } else {
+                popup.classList.add('reviews-popup_active');
+                popup.addEventListener('click', popup => {
+                    if (popup.target.classList.contains('reviews-popup_active')) {
+                        popup.target.classList.remove('reviews-popup_active');
+                    }
+                })
+                const indexOfSlide = +this.getAttribute('data-number') - 1;
+                swiper.slideTo(indexOfSlide);
+            }
+        })
+    })
+}
+
+initSwiperPopup();
 
 stickyHeader()
